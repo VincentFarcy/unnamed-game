b524708183c17b92b7003fdc69ae0ac2d3fc1c2a
VincentFarcy <v.farcy72@gmail.com>
2020-04-12 16:33:44 +0200

    user controller : add and delete methods

--- back/src/Controller/UserController.php
+++ back/src/Controller/UserController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
 /**
@@ -66,6 +68,73 @@ class UserController extends AbstractController
             $user->setPseudo($pseudo);
         }
         $user->setUpdatedAt(new \DateTime());
+        $em->flush();
+
+        // Send new user data
+        return $this->json([
+            'user' => 
+                $serializer->normalize(
+                    $user,
+                    null, ['groups' => ['user']]
+                ), 
+        ]);
+    }
+
+    /**
+     * @Route("add", name="add", methods={"POST"})
+     */
+    public function add(
+        Request $request,
+        EntityManagerInterface $em,
+        SerializerInterface $serializer,
+        UserPasswordEncoderInterface $passwordEncoder)
+    {
+        // Get data from client request
+        $content = $request->getContent();
+        // json to array
+        $data = json_decode($content, true);
+        // extract array into distinct variables
+        extract($data);
+
+        // check of required data
+        if (    !isset($email)
+            ||  !isset($pseudo)
+            ||  !isset($password)
+            ||  !isset($ageChecked)
+        ) {
+            // error 400
+            return $this->json(
+                 [
+                    "message" => "add user error : missing data",
+                    "data" => $data
+                ],
+                JsonResponse::HTTP_BAD_REQUEST
+            );
+        }
+
+        // check age confirmation
+        if (!$ageChecked) {
+            // error 400
+            return $this->json(
+                 [
+                    "message" => "add user error : age confirmation missing",
+                    "data" => $data
+                ],
+                JsonResponse::HTTP_BAD_REQUEST
+            );
+        }
+
+        // Create a new user ()
+        $user = new User();
+
+        // Add user data in DB with encoded password
+        $encodedPassword = $passwordEncoder->encodePassword($user, $password);
+        $user
+            ->setEmail($email)
+            ->setPseudo($pseudo)
+            ->setRoles(["ROLE_USER"])
+            ->setPassword($encodedPassword)
+            ->setAgeChecked($ageChecked);
         $em->persist($user);
         $em->flush();
 
@@ -80,20 +149,22 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("add", name="add")
-     */
-    public function add()
-    {
-        return $this->json('');
-    }
-
-    /**
-     * @Route("delete", name="delete")
+     * @Route("delete", name="delete", methods={"DELETE"})
      */
     public function delete(
         EntityManagerInterface $em)
     {
         $user = $this->GetUser();
-        return $this->json('');
+
+        $em->remove($user);
+        $em->flush();
+
+        return $this->json(
+            [
+               "message" => "User deleted",
+            ]
+        );
+
+        return $this->redirectToRoute('admin_person_index');
     }
 }
