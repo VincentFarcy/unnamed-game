712bce77066aad854f388589a0ae46244324183e
VincentFarcy <v.farcy72@gmail.com>
2020-04-12 15:02:22 +0200

    User Controller : read and edit methods

---
+++ back/src/Controller/UserController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * @Route("/api/user/", name="api_user_")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("read", name="read", methods={"GET"})
+     */
+    public function read(
+        SerializerInterface $serializer)
+    {
+        // Get connected user
+        $user = $this->GetUser();
+        // Send data
+        return $this->json([
+            'user' => 
+                $serializer->normalize(
+                    $user,
+                    null, ['groups' => ['user']]
+                ), 
+        ]);
+    }
+
+    /**
+     * @Route("edit", name="edit", methods={"PUT"})
+     */
+    public function edit(
+        Request $request,
+        EntityManagerInterface $em,
+        SerializerInterface $serializer)
+    {
+        // Get data from client request
+        $content = $request->getContent();
+        // json to array
+        $data = json_decode($content, true);
+        // extract array into distinct variables
+        extract($data);
+
+        // Get connected user
+        $user = $this->GetUser();
+
+        // Check sended data
+        if (!isset($username)) {
+            // error 400
+            return $this->json(
+                [
+                   "message" => "Edit user error : no data received",
+               ],
+               JsonResponse::HTTP_BAD_REQUEST
+           );
+        }
+
+        // Update user data in DB for sent data
+        if (isset($username)) {
+            $user->setUsername($username);
+        }
+        $user->setUpdatedAt(new \DateTime());
+        $em->persist($user);
+        $em->flush();
+
+        // Send new user data
+        return $this->json([
+            'user' => 
+                $serializer->normalize(
+                    $user,
+                    null, ['groups' => ['user']]
+                ), 
+        ]);
+    }
+
+    /**
+     * @Route("add", name="add")
+     */
+    public function add()
+    {
+        return $this->json('');
+    }
+
+    /**
+     * @Route("delete", name="delete")
+     */
+    public function delete(
+        EntityManagerInterface $em)
+    {
+        $user = $this->GetUser();
+        return $this->json('');
+    }
+}
