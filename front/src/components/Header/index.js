411304f906d79475389c0900e58e1c9b59bf95bd
AnnSo-B <as.baujard.33@gmail.com>
2020-04-14 14:05:56 +0200

    WIP : mobile navigation

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -1,16 +1,41 @@
 // == Import npm
 import React from 'react';
-import PropTypes from 'prop-types';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
 
 // == Import local
 import './style.scss';
+import logo from '../../assets/images/Unnamed_Game_3_white.svg'
 
 // == Component
 const Header = () => (
   <header className="header">
-    Header
+    {/* https://react-bootstrap.github.io/components/navbar/#navbars-overview */}
+    <Navbar expand="lg" variant="dark">
+      {/* Logo */}
+      <Navbar.Brand href="/">
+        <img src={logo} alt="unnamed game logo" />
+      </Navbar.Brand>
+      {/* Burger menu */}
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        {/* Pages */}
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Accueil</Nav.Link>
+          <Nav.Link href="#">Règles générales</Nav.Link>
+          <Nav.Link href="#">Présentation de l'équipe</Nav.Link>
+          <Nav.Link href="#">Remerciements</Nav.Link>
+        </Nav>
+        {/* items to be displayed depending on user's login status */}
+        {/* https://react-bootstrap.github.io/components/buttons/ 
+        https://react-bootstrap.github.io/components/buttons/#button-props*/}
+      </Navbar.Collapse>
+    </Navbar>
   </header>
 );
+
+
 
 // == Props validation
 
