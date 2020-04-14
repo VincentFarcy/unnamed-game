693a28083b2d1fda236d13019e7565d67a34b51f
AnnSo-B <as.baujard.33@gmail.com>
2020-04-14 11:00:12 +0200

    mobile app structure-ter programming

---
+++ front/src/components/Header/index.js
@@ -0,0 +1,18 @@
+// == Import npm
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// == Import local
+import './style.scss';
+
+// == Component
+const Header = () => (
+  <header className="header">
+    Header
+  </header>
+);
+
+// == Props validation
+
+// == Export
+export default Header;
---
+++ front/src/components/MainPlay/index.js
@@ -0,0 +1,18 @@
+// == Import npm
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// == Import local
+import './style.scss';
+
+// == Component
+const MainPlay = () => (
+  <div>
+    MainPlay
+  </div>
+);
+
+// == Props validation
+
+// == Export
+export default MainPlay;
