666e0c7f161f9b82d1f7e1533f0ddc7ec68de892
Kharmaska - Vincent M <kharmachaitea@gmail.com>
2020-04-16 15:09:23 +0200

    Create the PlayerInfo bar (Compo+Container) that appears inside the Reward page

---
+++ front/src/containers/PlayerInfo.js
@@ -0,0 +1,22 @@
+// npm imports
+import { connect } from 'react-redux';
+
+// local imports
+import PlayerInfo from '../components/PlayerInfo';
+
+// state
+const mapStateToProps = (state) => ({
+  // TODO: Handle the counter from state
+  mainCounter: 10,
+  // TODO: Handle the Health Points from state
+  playerHp: ((
+    // HP = ((Volonté / 2) + Consitution) * 10
+    state.gameplay.abilities[3].value / 2) + (state.gameplay.abilities[2].value)) * 10,
+});
+
+// actions
+const mapDispatchToProps = {};
+
+
+// export
+export default connect(mapStateToProps, mapDispatchToProps)(PlayerInfo);
