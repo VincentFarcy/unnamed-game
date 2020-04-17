73ca9b64588eda35eeb82cb332de0b59bf30fa5e
Kharmaska - Vincent M <kharmachaitea@gmail.com>
2020-04-17 16:07:36 +0200

    Functionnal Container for Rewards with full dice rolls randomization handling

---
+++ front/src/func.js
@@ -0,0 +1,12 @@
+function rollDice(min, max) {
+  return min + Math.floor(Math.random() * (max - min + 1));
+}
+// ==== Examples of Roll Dice ===
+// const rollDice4 = () => rollDice(1, 4);
+// const rollDice6 = () => rollDice(1, 6);
+// const rollDice8 = () => rollDice(1, 8);
+// const rollDice12 = () => rollDice(1, 12);
+// const rollDice20 = () => rollDice(1, 20);
+
+
+export default rollDice;
