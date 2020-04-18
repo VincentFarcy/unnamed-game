026b7b7fd92e725007b4c827139d5ac3742fe238
AnnSo-B <as.baujard.33@gmail.com>
2020-04-18 18:41:01 +0200

    fix proptypes problem

--- front/src/components/LinkButton/index.js
+++ front/src/components/LinkButton/index.js
@@ -6,12 +6,11 @@ import PropTypes from 'prop-types';
 // == Import local
 
 // == Component
-const LinkButton = ({ cssClassName, buttonName, url = '', onClick= '' }) => (
+const LinkButton = ({ cssClassName, buttonName, url = '' }) => (
   <NavLink
     exact
     to={url}
     className={`${cssClassName} nav-link btn btn-outline-light`}
-    onClick={onClick}
   >
     {buttonName}
   </NavLink>
