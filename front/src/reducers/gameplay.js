c1aedc3df4d39545e4c7ceaf1093534df05b401d
Johan .S <j.santaliestra@gmail.com>
2020-04-21 12:17:48 +0200

    update gameplay reducer (updating player objet)

--- front/src/reducers/gameplay.js
+++ front/src/reducers/gameplay.js
@@ -107,11 +107,11 @@ const gameplay = (state = initialState, action = {}) => {
           ...state.player,
           playerTotalHP: ((state.abilities[3].value / 2) + (state.abilities[2].value)) * 10,
           playerCurrentHP: ((state.abilities[3].value / 2) + (state.abilities[2].value)) * 10,
-          baseToHit: ((state.abilities[1].value) + Math.floor((state.abilities[4].value / 3))),
-          baseAvoid: ((state.abilities[1].value) + Math.floor((state.abilities[4].value / 2))),
+          baseTouch: ((state.abilities[1].value) + Math.floor((state.abilities[4].value / 3))),
+          dodge: ((state.abilities[1].value) + Math.floor((state.abilities[4].value / 2))),
           baseDamage: state.abilities[0].value,
-          baseInitiative: state.abilities[1].value,
-          healing:  Math.floor(((state.abilities[3].value / 2) + (state.abilities[4].value / 2))),
+          baseSpeed: state.abilities[1].value,
+          baseHealing:  Math.floor(((state.abilities[3].value / 2) + (state.abilities[4].value / 2))),
         },
       };
     case DECREMENT_CREATE_CHARACTER:
@@ -122,11 +122,11 @@ const gameplay = (state = initialState, action = {}) => {
           ...state.player,
           playerTotalHP: ((state.abilities[3].value / 2) + (state.abilities[2].value)) * 10,
           playerCurrentHP: ((state.abilities[3].value / 2) + (state.abilities[2].value)) * 10,   
-          baseToHit: ((state.abilities[1].value) + Math.floor((state.abilities[4].value / 3))),
-          baseAvoid: ((state.abilities[1].value) + Math.floor((state.abilities[4].value / 2))),
+          baseTouch: ((state.abilities[1].value) + Math.floor((state.abilities[4].value / 3))),
+          dodge: ((state.abilities[1].value) + Math.floor((state.abilities[4].value / 2))),
           baseDamage: state.abilities[0].value,
-          baseInitiative: state.abilities[1].value,
-          healing: Math.floor(((state.abilities[3].value / 2) + (state.abilities[4].value / 2))),
+          baseSpeed: state.abilities[1].value,
+          baseHealing: Math.floor(((state.abilities[3].value / 2) + (state.abilities[4].value / 2))),
         },
       };
     case FIND_OPPONENT:
