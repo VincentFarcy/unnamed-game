7c40dce35577f83d603c3afadb76ff4890f007d0
Johan .S <j.santaliestra@gmail.com>
2020-04-24 19:55:47 +0200

    update text for Nothing / Event Win / Event Fail

--- front/src/components/Rest/index.js
+++ front/src/components/Rest/index.js
@@ -10,6 +10,7 @@ import './style.scss';
 const Rest = ({ actionRest }) => (
   <div className="main__play">
     <p className="rest__title">Vous décidez de vous reposer</p>
+    <p className="rest__texte">Vous restez tout de même sur vos gardes, il y a de nombreux bruits étranges sur le vaisseau... au loin vous pensez attendre une porte s'ouvrir... plus le temps passe plus vous avez l'impression de devenir fou ici. Vous avez récupérez quelques points de vies grâce à ce repos. Seulement le temps passe, et cela ne joue pas en votre faveur.</p>
     <div className="button__container">
       <Button cssClassName="next__button" buttonName="Se reposer" url="/play/sequence" onClick={actionRest} />
     </div>
