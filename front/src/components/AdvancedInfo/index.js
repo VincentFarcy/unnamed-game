776a08cbdd095d8b7a26f935005c58b4d7c82cf2
Kharmaska - Vincent M <kharmachaitea@gmail.com>
2020-04-24 10:17:18 +0200

    quick fix on some French syntax on the Combat window

--- front/src/components/AdvancedInfo/index.js
+++ front/src/components/AdvancedInfo/index.js
@@ -13,10 +13,11 @@ import './style.scss';
 const AdvancedInfo = ({ abilities, baseToHit, baseAvoid, baseDamage, baseInitiative, healing }) => (
   <div className="AdvancedInfo__container">
     <div className="MainAbilities__container">
+    {/* == Map handling the display of the basic characteristics like Strength, Agility, etc.*/}
       {
         abilities.map((ability) => (
           <span key={uuidv4()}>
-            {ability.name}:{ability.value}
+            {ability.name} : {ability.value}
           </span>
         ))
       }
@@ -31,7 +32,7 @@ const AdvancedInfo = ({ abilities, baseToHit, baseAvoid, baseDamage, baseInitiat
           </Tooltip>
         }
       >
-        <span>Esquive: {baseAvoid}</span>
+        <span>Esquive : {baseAvoid}</span>
       </OverlayTrigger>
       <OverlayTrigger
         placement="top"
@@ -42,7 +43,7 @@ const AdvancedInfo = ({ abilities, baseToHit, baseAvoid, baseDamage, baseInitiat
           </Tooltip>
         }
       >
-        <span>Initiative: {baseInitiative + 1} - {baseInitiative + 10}</span>
+        <span>Initiative : {baseInitiative + 1} - {baseInitiative + 10}</span>
       </OverlayTrigger>
       <OverlayTrigger
         placement="top"
@@ -53,7 +54,7 @@ const AdvancedInfo = ({ abilities, baseToHit, baseAvoid, baseDamage, baseInitiat
           </Tooltip>
         }
       >
-        <span>Toucher: {baseToHit + 1} - {baseToHit + 6}</span>
+        <span>Toucher : {baseToHit + 1} - {baseToHit + 6}</span>
       </OverlayTrigger>
       <OverlayTrigger
         placement="top"
@@ -64,7 +65,7 @@ const AdvancedInfo = ({ abilities, baseToHit, baseAvoid, baseDamage, baseInitiat
           </Tooltip>
         }
       >
-        <span>Dégâts: {baseDamage + 1} - {baseDamage + 4}</span>
+        <span>Dégâts : {baseDamage + 1} - {baseDamage + 4}</span>
       </OverlayTrigger>
       <OverlayTrigger
         placement="top"
@@ -75,7 +76,7 @@ const AdvancedInfo = ({ abilities, baseToHit, baseAvoid, baseDamage, baseInitiat
           </Tooltip>
         }
       >
-        <span>Guérison: {healing}</span>
+        <span>Guérison : {healing}</span>
       </OverlayTrigger>
     </div>
   </div>
