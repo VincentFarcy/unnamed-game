6495f353106a972d94971653cc85fe6cb6603f8e
AnnSo-B <as.baujard.33@gmail.com>
2020-04-24 14:50:08 +0200

    cap the number of life points recovered after resting

--- front/src/reducers/gameplay.js
+++ front/src/reducers/gameplay.js
@@ -330,7 +330,7 @@ const gameplay = (state = initialState, action = {}) => {
         player: {
           ...state.player,
           playerTotalHP: ((state.player.abilities[3].value / 2) + (state.player.abilities[2].value)) * 10,
-          playerCurrentHP: ((state.player.abilities[3].value / 2) + (state.player.abilities[2].value)) * 10,     
+          playerCurrentHP: ((state.player.abilities[3].value / 2) + (state.player.abilities[2].value)) * 10,
           hacking: ((state.player.abilities[4].value) + Math.floor((state.player.abilities[3].value / 3))),
           baseTouch: ((state.player.abilities[1].value) + Math.floor((state.player.abilities[4].value / 3))),
           dodge: ((state.player.abilities[1].value) + Math.floor((state.player.abilities[4].value / 2))),
@@ -454,7 +454,9 @@ const gameplay = (state = initialState, action = {}) => {
         },
         player: {
           ...state.player,
-          playerCurrentHP: (state.player.playerCurrentHP + state.player.baseHealing + rollDice(1, 2)),
+          playerCurrentHP:
+            (state.player.playerCurrentHP + state.player.baseHealing + rollDice(1, 2)) < state.player.playerTotalHP ?
+              state.player.playerCurrentHP + state.player.baseHealing + rollDice(1, 2) : state.player.playerTotalHP,
         },
       };
     case MEDIC_ACTION:
