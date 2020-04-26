d57aef76eff3161a44f28ad5f9c99d20414e25e9
AnnSo-B <as.baujard.33@gmail.com>
2020-04-26 10:54:49 +0200

    update text on Event

--- front/src/components/Event/index.js
+++ front/src/components/Event/index.js
@@ -20,14 +20,14 @@ const Event = ({
 
   return (
     <div className="main__play">
-      <p className="event__title">Exploration</p>
+      <p className="event__title">Exploration - Hacking</p>
       {win ? (
         <>
           <WonEvent />
         </>
       ) : (
         <>
-          <p className="event__fail__text">Tu as perdu l'évenement aléatoire</p>
+          <p className="event__fail__text">Malheureusement vous restez dans l'incapacité d'outre-passer le firewall. Vous vous sentez comme un dev junior qui découvre React-Redux pour la 1ère fois.</p>
           <div className="button__container">
             <Button cssClassName="next__button" buttonName="Suivant" url="/play/sequence" onClick={updateTimer} />
           </div>
