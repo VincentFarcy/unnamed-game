b7c02c7eec58223fdf49035e0f244a2a0c48572b
VincentFarcy <v.farcy72@gmail.com>
2020-04-30 15:18:05 +0200

    resize img on combat window for souris avatar

--- front/src/components/Combat/OpponentCombatInfo.js
+++ front/src/components/Combat/OpponentCombatInfo.js
@@ -29,62 +29,63 @@ const OpponentCombatInfo = ({ currentOpponent }) => {
 
   return (
     <div className="combat-info">
-      {findImage === 'drone' ? <Image className="combat-info__avatar" src={drone} rounded alt="drone" /> : ''}
-      {findImage === 'bat' ? <Image className="combat-info__avatar" src={bat} rounded alt="bat" /> : ''}
-      {findImage === 'goblin' ? <Image className="combat-info__avatar" src={goblin} rounded alt="goblin" /> : ''}
-      {findImage === 'wolf' ? <Image className="combat-info__avatar" src={wolf} rounded alt="wolf" /> : ''}
-      {findImage === 'souris' ? (
-        <OverlayTrigger
-          placement="top"
-          delay={{ show: 300, hide: 300 }}
-          overlay={
-            (
-              <Tooltip id="tooltip">
-                Vous avez rattrapé votre avance... OUI, OUI, OUI!
-              </Tooltip>
-            )
-          }
-        >
-          <Image className="combat-info__avatar" src={ratage} rounded alt="ratage" />
-        </OverlayTrigger>
-      ) : ''}
-      {findImage === 'megabot' ? (
-        <OverlayTrigger
-          placement="top"
-          delay={{ show: 300, hide: 300 }}
-          overlay={
-            (
-              <Tooltip id="tooltip">
-                Boss de fin ?!
-              </Tooltip>
-            )
-          }
-        >
-          <Image className="combat-info__avatar" src={megabot} rounded alt="megabot" />
-        </OverlayTrigger>
-      ) : ''}
-      {findImage === 'rat' ? <Image className="combat-info__avatar" src={rat} rounded alt="rat" /> : ''}
-      {findImage === 'robotMonowheel' ? <Image className="combat-info__avatar" src={robotMonowheel} rounded alt="robot1" /> : ''}
-      {findImage === 'robotVintage' ? <Image className="combat-info__avatar" src={robotVintage} rounded alt="robot2" /> : ''}
-      {findImage === 'sabine'
-        ? (
+      <div className="combat-info__opponent-image-container">
+        {findImage === 'drone' ? <Image className="combat-info__avatar" src={drone} rounded alt="drone" /> : ''}
+        {findImage === 'bat' ? <Image className="combat-info__avatar" src={bat} rounded alt="bat" /> : ''}
+        {findImage === 'goblin' ? <Image className="combat-info__avatar" src={goblin} rounded alt="goblin" /> : ''}
+        {findImage === 'wolf' ? <Image className="combat-info__avatar" src={wolf} rounded alt="wolf" /> : ''}
+        {findImage === 'souris' ? (
           <OverlayTrigger
             placement="top"
             delay={{ show: 300, hide: 300 }}
             overlay={
               (
                 <Tooltip id="tooltip">
-                  Le vaisseau tremble, la foudre a déjà frappé 3 fois...
+                  Vous avez rattrapé votre avance... OUI, OUI, OUI!
                 </Tooltip>
               )
             }
           >
-            <Image className="combat-info__avatar" src={sabine} rounded alt="sabine" />
+            <Image className="combat-info__avatar combat-info__avatar--souris" src={ratage} rounded alt="ratage" />
           </OverlayTrigger>
         ) : ''}
-      {findImage === 'slime' ? <Image className="combat-info__avatar" src={slime} rounded alt="slime" /> : ''}
-      {findImage === 'spider' ? <Image className="combat-info__avatar" src={spider} rounded alt="spider" /> : ''}
-
+        {findImage === 'megabot' ? (
+          <OverlayTrigger
+            placement="top"
+            delay={{ show: 300, hide: 300 }}
+            overlay={
+              (
+                <Tooltip id="tooltip">
+                  Boss de fin ?!
+                </Tooltip>
+              )
+            }
+          >
+            <Image className="combat-info__avatar" src={megabot} rounded alt="megabot" />
+          </OverlayTrigger>
+        ) : ''}
+        {findImage === 'rat' ? <Image className="combat-info__avatar" src={rat} rounded alt="rat" /> : ''}
+        {findImage === 'robotMonowheel' ? <Image className="combat-info__avatar" src={robotMonowheel} rounded alt="robot1" /> : ''}
+        {findImage === 'robotVintage' ? <Image className="combat-info__avatar" src={robotVintage} rounded alt="robot2" /> : ''}
+        {findImage === 'sabine'
+          ? (
+            <OverlayTrigger
+              placement="top"
+              delay={{ show: 300, hide: 300 }}
+              overlay={
+                (
+                  <Tooltip id="tooltip">
+                    Le vaisseau tremble, la foudre a déjà frappé 3 fois...
+                  </Tooltip>
+                )
+              }
+            >
+              <Image className="combat-info__avatar" src={sabine} rounded alt="sabine" />
+            </OverlayTrigger>
+          ) : ''}
+        {findImage === 'slime' ? <Image className="combat-info__avatar" src={slime} rounded alt="slime" /> : ''}
+        {findImage === 'spider' ? <Image className="combat-info__avatar" src={spider} rounded alt="spider" /> : ''}
+      </div>
       <ProgressBar variant="success" min={0} max={currentOpponent.health} now={now} label={`HP: ${now}/${currentOpponent.health}`} />
     </div>
   );
